eaf39f5cefd66f6125d34bcc9fccd2c2b68cea53
mewiebe <mwalz2@gmail.com>
2021-05-16 12:38:58 -0700

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Sentiment analysis of tweets about different airlines in the USA. ',
+    author='Maureen Wiebe',
+    license='MIT',
+)
